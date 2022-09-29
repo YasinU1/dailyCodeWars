597ff141d38c32f2fc40c3e92a259321c45d06ed
YasinU1 <sayedmuhammadyasin.usman@vodafone.com>
2022-09-29 08:40:19 +0100

    Turnery operator and Conditionals

---
+++ day6- 8kyu Is he going to survive?.js
@@ -0,0 +1,22 @@
+
+
+
+
+function hero(bullets, dragons){
+    //Get Coding!
+      return (dragons * 2) - bullets <= 0 ? true : false
+    
+    }
+    
+function hero(bullets, dragons){
+    //Get Coding!
+    let bulletPerDragon = dragons + dragons
+    if ((bulletPerDragon - bullets) <= 0){
+        return true
+    }
+    else{
+        return false
+    }
+}
+    
+
