b22713892ee3de8ae8bbf185550a0d38abebc377
YasinU1 <30217276@blackpool.ac.uk>
2022-10-09 18:42:37 +0100

    arr.sort()  Math.min, arr.map()

--- day14-7kyu Shortest Word.js
+++ day14-7kyu Shortest Word.js
@@ -9,3 +9,8 @@ function findShort(s){
     let shortestArr = arr.sort((a,b) => a.length > b.length ? 1 : -1)
     return shortestArr[0].length
   }
+
+
+function findShort(s){
+    return Math.min(...s.split(" ").map (s => s.length));
+}
