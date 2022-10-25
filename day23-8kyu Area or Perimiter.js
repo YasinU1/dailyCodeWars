8b75c7d7b8e40f330040302b212c1497e485ddb0
YasinU1 <30217276@blackpool.ac.uk>
2022-10-25 22:31:05 +0100

    PREP condtional turnery

--- day23-8kyu Area or Perimiter.js
+++ day23-8kyu Area or Perimiter.js
@@ -33,26 +33,3 @@ const areaOrPerimeter = function(l , w) {
 areaOrPerimeter(6,6)  // 36 area 6x6
 areaOrPerimeter(5,7) // 24
 areaOrPerimeter(9, 10) // 38 9+9 10+ 10
-
-// PREP
-// month as an integer 1-12, month 2 = 1st , month 6 is 2
-//P:  - is it a number, will it every be a string boolean , always be a whole number and not a float with decimals, will it be positive,
-// will there be anything that might be passed in that i havent clarified
-//P: num, whole, + 
-//R: Return or Console.log/  - return of each number
-//Examples
-
-function quarterOf(month) {
-    // math.ceil month / 3
-    // return what month it is
-    return Math.ceil(month / 3)
-    
-  }
-  
-  // Going to create test cases so that we are 100% aligned with watch the results should be 
-  quarterOf(2) // 1   so our argument of 2 with it being feb will produce the number 1 because its in the first quarter
-  quarterOf(6) // 2 
-  quarterOf(11) // 4
-  
-// does this look correct in what we expecting to come out of this problem , reiterate that result is a number whole and positive -- 
-// does this look good
