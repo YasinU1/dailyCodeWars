db9c8da275d01e5233e428e0960c2a7950b4eb1d
sayedmuhammadyasin-usman <sayedmuhammadyasin.usman@vodafone.com>
2023-07-15 16:48:31 +0100

    Added day1.0 Leetcode Two Sum

--- day1.0-TwoSum.js
+++ day1.0-TwoSum.js
@@ -1,27 +1,29 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
 
 // given array of integers, and given a target, 
 // return indices of two numbers to == target
 
 var twoSum = function(nums, target) {
     /// go through the array for the first time, for a full length of the array
+    let hashmap = {}
     for (let i = 0; i < nums.length; i++){
-        for (let j = 1; j < nums.length + 1; i++){
-            if (nums[i] + nums[j] == target){
-                return [i,j]
-            }
+        let diff = target - nums[i]
+        if (hashmap.hasOwnProperty(diff)){
+            return [hashmap[diff], i]
         }
+        hashmap[nums[i]] = i
     }
 
     // go through the array for the second time but not include the first no.
     // add the second time to the first time,
     // compare if the add == target, if not contiue
     // if that has passed, return a array with the index values of the two correct values
-
+    // for (let i = 0; i < nums.length; i++){
+    //     for (let j = 1; j < nums.length + 1; i++){
+    //         if (nums[i] + nums[j] == target){
+    //             return [i,j]
+    //         }
+    //     }
+    // }
 
 
 };
